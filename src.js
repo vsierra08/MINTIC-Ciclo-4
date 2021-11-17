e427e7c8dc4587bc90ebaf15e69db63a9ec6a089
Victor O Sierra F <43505962+vsierra08@users.noreply.github.com>
2021-11-17 00:13:31 -0500

    Add files via upload

--- src.js
+++ src.js
@@ -7,22 +7,25 @@ const registrarMunicipio = (
     numeroHombres,
     numeroRecuperados
 ) => {
-    if ((numeroMujeres + numeroHombres) == numeroCasos) {
-
-    } else {
+    if (numeroMujeres + numeroHombres == numeroCasos) {} else {
         datos = ["Algeciras", "Huila", "2342", "342", "2000", "2124"];
     }
-
 };
 
 const noticias = [];
 const filtrarEInsertarNoticias = (titulo, contenido, url) => {
-
+    if (titulo == "coronavirus" || titulo == "covid19") {
+        noticias = [
+            "titulo de noticia de covid19",
+            "contenido de la noticia de coronavirus",
+            "https://path-to-noticia",
+        ];
+    }
 };
 
 const borrarNoticias = () => {
     noticias.length = 0;
-    localStorage.setItem('noticias', null);
+    localStorage.setItem("noticias", null);
 };
 module.exports.filtrarEInsertarNoticias = filtrarEInsertarNoticias;
 module.exports.borrarNoticias = borrarNoticias;
