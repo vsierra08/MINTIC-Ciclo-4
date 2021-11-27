c5de72347d4eeb9126d5239d98dd5e8a9f96ed60
Victor O Sierra F <43505962+vsierra08@users.noreply.github.com>
2021-11-26 19:53:32 -0500

    10 / 10 reto 3
    10 / 10 reto 3  ajustes al src.js y creacion de news.json

--- src.js
+++ src.js
@@ -1,5 +1,4 @@
-const datos = [];
-const registrarMunicipio = (
+const registrarMunicipio = async(
     municipio,
     departamento,
     numeroCasos,
@@ -7,38 +6,45 @@ const registrarMunicipio = (
     numeroHombres,
     numeroRecuperados
 ) => {
-    if (numeroMujeres + numeroHombres > numeroCasos) {
-        throw new Error();
+    let response = await fetch(
+        "https://misiontic2022upb.vercel.app/api/epidemiologic-monitor/villages", {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                municipio: municipio,
+                departamento: departamento,
+                numeroCasos: numeroCasos,
+                numeroMujeres: numeroMujeres,
+                numeroHombres: numeroHombres,
+                numeroRecuperados: numeroRecuperados,
+            }),
+        }
+    );
+    let result = await response.json();
+    return result;
+};
+const filtrarEInsertarNoticias = async() => {
+    let response = await fetch(
+        "https://misiontic2022upb.vercel.app/api/epidemiologic-monitor/news"
+    );
+    let noticiasAPI = await response.json();
+    const noticiasFiltradas = [];
+    for (let i = 0, h = 0, l = noticiasAPI.length; i < l; i++) {
+        if (
+            noticiasAPI[i].titulo.indexOf("coronavirus") !== -1 ||
+            noticiasAPI[i].titulo.indexOf("covid19") !== -1 ||
+            noticiasAPI[i].contenido.indexOf("coronavirus") !== -1 ||
+            noticiasAPI[i].contenido.indexOf("covid19") !== -1
+        ) {
+            noticiasFiltradas[h] = noticiasAPI[i];
+            h += 1;
+        }
     }
-    var data = {
-        municipio: municipio,
-        departamento: departamento,
-        numeroCasos: numeroCasos,
-        numeroMujeres: numeroMujeres,
-        numeroHombres: numeroHombres,
-        numeroRecuperados: numeroRecuperados,
-    };
-    datos.push(data);
-    localStorage.setItem("datos", JSON.stringify(datos));
+    return noticiasFiltradas;
 };
-const noticias = [];
-const filtrarEInsertarNoticias = (titulo, contenido, url) => {
-    if (
-        titulo.indexOf("coronavirus") >= 0 ||
-        titulo.indexOf("covid19") >= 0 ||
-        contenido.indexOf("coronavirus") >= 0 ||
-        contenido.indexOf("covid19") >= 0
-    ) {
-        var o = { titulo: titulo, contenido: contenido, url: url };
-        console.log("Será insertado=>" + titulo + ":" + contenido);
-        noticias.push(o);
-        localStorage.setItem("noticias", JSON.stringify(noticias));
-    }
-};
-const borrarNoticias = () => {
-    noticias.length = 0;
-    localStorage.setItem("noticias", JSON.stringify(noticias));
-};
+
 module.exports.filtrarEInsertarNoticias = filtrarEInsertarNoticias;
-module.exports.borrarNoticias = borrarNoticias;
 module.exports.registrarMunicipio = registrarMunicipio;
