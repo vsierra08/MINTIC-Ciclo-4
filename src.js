27ba45ca1c7603ff76232e4e9d4a9042fbcf76eb
Victor O Sierra F <43505962+vsierra08@users.noreply.github.com>
2021-12-07 14:40:28 -0500

    10/10 actualizacion sprint 5
    10/10 actualizacion sprint 5

--- src.js
+++ src.js
@@ -1,90 +1,54 @@
-const express = require("express");
-const app = express();
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.get("/api/epidemiologic-monitor/news", (req, res) => {
-    res.send(news);
-});
-app.get("/api/epidemiologic-monitor/villages", (req, res) => {
-    res.send(villages);
-});
-app.post("/api/epidemiologic-monitor/villages", (req, res) => {
-    console.log(req.body);
-    villages.push(req.body);
-    res.send(villages);
-});
 
 
-var villages = [{
-        "id": 1,
-        "municipio": "Medellín",
-        "departamento": "Antioquia",
-        "numeroCasos": 412988,
-        "numeroMujeres": 220818,
-        "numeroHombres": 192170,
-        "numeroRecuperados": 401502
-    },
+const registrarMunicipio = async (
+  municipio,
+  departamento,
+  numeroCasos,
+  numeroMujeres,
+  numeroHombres,
+  numeroRecuperados
+) => {
+  let response = await fetch(
+    "https://misiontic2022upb.vercel.app/api/epidemiologic-monitor/villages",
     {
-        "id": 2,
-        "municipio": "Guarne",
-        "departamento": "Antioquia",
-        "numeroCasos": 5210,
-        "numeroMujeres": 2709,
-        "numeroHombres": 2501,
-        "numeroRecuperados": 5065
-    },
-    {
-        "id": 3,
-        "municipio": "Itaguí",
-        "departamento": "Antioquia",
-        "numeroCasos": 49044,
-        "numeroMujeres": 25953,
-        "numeroHombres": 23091,
-        "numeroRecuperados": 48114
-    },
-    {
-        "id": 4,
-        "municipio": "Turbaco",
-        "departamento": "Antioquia",
-        "numeroCasos": 49044,
-        "numeroMujeres": 25953,
-        "numeroHombres": 23091,
-        "numeroRecuperados": 48114
-    },
-    {
-        "id": 5,
-        "municipio": "Bello",
-        "departamento": "Antioquia",
-        "numeroCasos": 49044,
-        "numeroMujeres": 25953,
-        "numeroHombres": 23091,
-        "numeroRecuperados": 48114
+      method: "POST",
+      headers: {
+        "accept": "application/json",
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        municipio: municipio,
+        departamento: departamento,
+        numeroCasos: numeroCasos,
+        numeroMujeres: numeroMujeres,
+        numeroHombres: numeroHombres,
+        numeroRecuperados: numeroRecuperados
+      })
     }
-];
-var news = [{
-        "titulo": "Un título de covid19",
-        "contenido": "cualquier contenido",
-        "url": "http://url/noticia.php"
-    },
-    {
-        "titulo": "Otro título de coronavirus",
-        "contenido": "cualquier contenido",
-        "url": "http://url/noticia.php"
-    },
-    {
-        "titulo": "Otro título de pandemia",
-        "contenido": "cualquier contenido de covid19",
-        "url": "http://url/noticia.php"
-    },
-    {
-        "titulo": "Otro título de futbol",
-        "contenido": "cualquier contenido con Messi",
-        "url": "http://url/noticia.php"
-    },
-    {
-        "titulo": "Otro título de fúlbol",
-        "contenido": "cualquier contenido que dice a a Messi le dió coronavirus",
-        "url": "http://url/noticia.php"
+  );
+  let result = await response.json();
+  return result;
+}
+
+
+const contadorNoticiasSPAM = async () => {
+
+  let response = await fetch("https://misiontic2022upb.vercel.app/api/epidemiologic-monitor/news");
+  let noticiasAPI = await response.json();
+  //console.log("noticiasAPI=" + noticiasAPI)
+  const noticiasFiltradas = []
+  const noticiasSpam = [];
+  for (const iterator of noticiasAPI) {
+    let covid19 = new RegExp("covid19");
+    let coronavirus = new RegExp("coronavirus");
+    if (iterator.titulo.search(covid19) != -1 || iterator.contenido.search(covid19) != -1 || iterator.titulo.search(coronavirus) != -1 || iterator.contenido.search(coronavirus) != -1) {
+      noticiasFiltradas.push(iterator);
+    } else {
+      noticiasSpam.push(iterator);
     }
-];
-module.exports = app;
+  }
+  return noticiasSpam.length;
+};
+
+module.exports.registrarMunicipio = registrarMunicipio;
+module.exports.contadorNoticiasSPAM = contadorNoticiasSPAM;
